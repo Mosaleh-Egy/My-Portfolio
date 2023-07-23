f2e47b701eb661807c263054938da3101587f318
mosalah <mohamedahmedsaleh50@gmail.com>
2023-07-23 13:04:40 -0700

    Updates

---
+++ precache-manifest.17aa41380ea809d61364031ddef532ff.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496c0eac54892a0adefaa748ae77b5eb",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "53583d2ea23d3135a83d",
+    "url": "/My-Portfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "63914dd39e181f8e1db1",
+    "url": "/My-Portfolio/static/css/main.3277f5ba.chunk.css"
+  },
+  {
+    "revision": "53583d2ea23d3135a83d",
+    "url": "/My-Portfolio/static/js/2.adef50de.chunk.js"
+  },
+  {
+    "revision": "a5f655d830d088aa9894c35a94634b59",
+    "url": "/My-Portfolio/static/js/2.adef50de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63914dd39e181f8e1db1",
+    "url": "/My-Portfolio/static/js/main.8723e8bd.chunk.js"
+  },
+  {
+    "revision": "aece559a475a96d54e58",
+    "url": "/My-Portfolio/static/js/runtime-main.49ea9518.js"
+  },
+  {
+    "revision": "63fcc00bda750ea64642b9e28292a8d7",
+    "url": "/My-Portfolio/static/media/Horison.63fcc00b.JPG"
+  },
+  {
+    "revision": "73bde4490e5db0a23f1cbccc30a607a1",
+    "url": "/My-Portfolio/static/media/abstract.73bde449.jpg"
+  },
+  {
+    "revision": "d719d42132491bd70da4346c2d51921a",
+    "url": "/My-Portfolio/static/media/avatar.d719d421.png"
+  },
+  {
+    "revision": "4bb62f3c2a6c2b09fc75d23eaa74fcfb",
+    "url": "/My-Portfolio/static/media/bootstrap-logo.4bb62f3c.png"
+  },
+  {
+    "revision": "4bab36220ef40b21fb4813b87ee2ecd4",
+    "url": "/My-Portfolio/static/media/codingQuiz.4bab3622.jpg"
+  },
+  {
+    "revision": "379fe880d7638d8d5e6493d9dcfac94c",
+    "url": "/My-Portfolio/static/media/jquery-logo.379fe880.png"
+  },
+  {
+    "revision": "86695d5d2f75c08615e5db9c648e099f",
+    "url": "/My-Portfolio/static/media/logo.86695d5d.png"
+  },
+  {
+    "revision": "8967ca67bbedb067ba0aedd74a407295",
+    "url": "/My-Portfolio/static/media/mern-logo.8967ca67.png"
+  },
+  {
+    "revision": "541dcec40560667c49cccd764392f3d4",
+    "url": "/My-Portfolio/static/media/mongodb-logo.541dcec4.png"
+  },
+  {
+    "revision": "96a28120ec1158dabbdaec6ef9e4d9b7",
+    "url": "/My-Portfolio/static/media/noteTaker.96a28120.JPG"
+  },
+  {
+    "revision": "fab0ce48802b61ee1a8c1fde080bb93a",
+    "url": "/My-Portfolio/static/media/passwordG.fab0ce48.JPG"
+  },
+  {
+    "revision": "19080b3cc1962fff3d289ffd360e640d",
+    "url": "/My-Portfolio/static/media/redux-logo.19080b3c.png"
+  },
+  {
+    "revision": "21812f6d8d4844da17453b80216a4445",
+    "url": "/My-Portfolio/static/media/sql-logo.21812f6d.png"
+  },
+  {
+    "revision": "ad8948b5210ddf13ec1f5e0d1d2e629d",
+    "url": "/My-Portfolio/static/media/tech-blog.ad8948b5.png"
+  },
+  {
+    "revision": "08f0c81fc056ac68a91d127aa3a13657",
+    "url": "/My-Portfolio/static/media/vuejs-logo.08f0c81f.png"
+  },
+  {
+    "revision": "f7099627bd7c464de1c0b40abf87ad86",
+    "url": "/My-Portfolio/static/media/webpack-logo.f7099627.png"
+  },
+  {
+    "revision": "4e0c2f6d0a4da47dac1896bcd0cef1b3",
+    "url": "/My-Portfolio/static/media/workDay.4e0c2f6d.JPG"
+  }
+]);
